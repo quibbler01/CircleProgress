60ed492af536819aec9d1479caf8b61b108e6aba
quibbler01 <a2639168845@163.com>
2022-07-31 21:02:44 +0800

    create CircleProgress view extends from View,overrid from constructors

---
+++ CircleProgress/src/main/java/cn/quibbler/circleprogress/CircleProgress.kt
@@ -0,0 +1,14 @@
+package cn.quibbler.circleprogress
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class CircleProgress : View {
+
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
+
+}
