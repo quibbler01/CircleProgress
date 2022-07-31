5f22d6e0505fb8e22752c168a2074c067ff41bad
quibbler01 <a2639168845@163.com>
2022-07-31 21:16:40 +0800

    delete useless libs,use androidx libs

--- demo/src/main/java/cn/quibbler/demo/DemoActivity.kt
+++ demo/src/main/java/cn/quibbler/demo/DemoActivity.kt
@@ -1,11 +1,13 @@
 package cn.quibbler.demo
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class DemoActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_demo)
     }
+
 }
