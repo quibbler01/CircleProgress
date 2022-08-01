fca0ff0ba6ca61678141292031695c51ac80e96e
quibbler01 <a2639168845@163.com>
2022-08-01 08:39:19 +0800

    modifiy constructor function

--- CircleProgress/src/main/java/cn/quibbler/circleprogress/CircleProgress.kt
+++ CircleProgress/src/main/java/cn/quibbler/circleprogress/CircleProgress.kt
@@ -6,9 +6,9 @@ import android.view.View
 
 class CircleProgress : View {
 
-    constructor(context: Context?) : super(context)
-    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+    constructor(context: Context?) : this(context, null)
+    constructor(context: Context?, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : this(context, attrs, defStyleAttr, 0)
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
 
 }
