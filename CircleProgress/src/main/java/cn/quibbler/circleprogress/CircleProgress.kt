f454a67ff56954714a821de736a732eae5214839
quibbler01 <a2639168845@163.com>
2022-08-02 08:19:51 +0800

    override onDraw() method to draw progress bar

--- CircleProgress/src/main/java/cn/quibbler/circleprogress/CircleProgress.kt
+++ CircleProgress/src/main/java/cn/quibbler/circleprogress/CircleProgress.kt
@@ -1,6 +1,7 @@
 package cn.quibbler.circleprogress
 
 import android.content.Context
+import android.graphics.Canvas
 import android.util.AttributeSet
 import android.view.View
 
@@ -9,6 +10,12 @@ class CircleProgress : View {
     constructor(context: Context?) : this(context, null)
     constructor(context: Context?, attrs: AttributeSet?) : this(context, attrs, 0)
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : this(context, attrs, defStyleAttr, 0)
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes) {
+
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+    }
 
 }
