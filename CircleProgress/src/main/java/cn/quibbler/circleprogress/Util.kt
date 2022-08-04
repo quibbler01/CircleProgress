4da453745f6828cf6631025fb7ddccae23249079
quibbler01 <a2639168845@163.com>
2022-08-04 08:37:19 +0800

    add Util file and function

---
+++ CircleProgress/src/main/java/cn/quibbler/circleprogress/Util.kt
@@ -0,0 +1,8 @@
+package cn.quibbler.circleprogress
+
+import android.content.Context
+
+public fun dip2px(context: Context, dpValue: Float): Int {
+    val scale = context.resources.displayMetrics.density
+    return (dpValue * scale + 0.5f).toInt()
+}
